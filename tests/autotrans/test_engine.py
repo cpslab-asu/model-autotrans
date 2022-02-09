c20c093d7ae354cfd05b8facba8d95963c3339cd
Quinn Thibeault <quinn.thibeault96@gmail.com>
2022-02-09 10:09:03 -0700

    Update engine test module to use data from file

--- tests/autotrans/test_engine.py
+++ tests/autotrans/test_engine.py
@@ -1,31 +1,49 @@
 import os.path as path
 
+import h5py
 import numpy as np
 import numpy.testing
-import scipy.io
-from autotrans.engine import Engine
+import pytest
+
+import autotrans.engine as engine
+
+INERTIA = 0.021991488283555904
 
 
-def test_engine_step():
-    engine = Engine(time_step_ms=40, engine_propeller_inertia=1.0, initial_rpm=0.0)
-    engine.step(throttle=0.35, impeller_torque=40.0)
-
-    assert engine.rpm == 1.0
+@pytest.fixture
+def trace_data() -> h5py.File:
+    return h5py.File(path.join(path.dirname(__file__), "test_data.h5"))
 
 
-def trace_data():
-    scipy.io.loadmat(path.join(path.dirname(__file__), "test_data.mat"))
+def test_engine_step(trace_data: h5py.File):
+    engine_rpm = trace_data["engine_rpm"]
+    throttle = trace_data["throttle"]
+    impeller_torque = trace_data["impeller_torque"]
+    expected_output = engine_rpm[1]
+
+    model = engine.Engine(
+        time_step_ms=40,
+        engine_propeller_inertia=INERTIA,
+        initial_rpm=engine_rpm[0]
+    )
+    model.step(throttle=throttle[0], impeller_torque=impeller_torque[0])
+
+    assert model.rpm == expected_output
 
 
-def test_engine_steps():
-    engine = Engine(time_step_ms=40, engine_propeller_inertia=1.0, initial_rpm=0.0)
-    throttle_signal = np.array([])
-    impeller_torque_signal = np.array([])
-    expected_output = np.array([])
+def test_engine_steps(trace_data: h5py.File):
+    throttle_trace: h5py.Dataset = trace_data["throttle"]
+    impeller_torque_trace: h5py.Dataset = trace_data["impeller_torque"]
+    engine_rpm: h5py.Dataset = trace_data["engine_rpm"]
+    model = engine.Engine(
+        time_step_ms=40,
+        engine_propeller_inertia=INERTIA,
+        initial_rpm=engine_rpm[0],
+    )
     output = []
 
-    for throttle, impeller_torque in zip(throttle_signal, impeller_torque_signal):
-        engine.step(throttle, impeller_torque)
-        output.append(engine.rpm)
+    for throttle, impeller_torque in zip(throttle_trace, impeller_torque_trace):
+        output.append(model.rpm)
+        model.step(throttle, impeller_torque)
 
-    assert np.testing.assert_equal(output, expected_output)
+    assert np.testing.assert_equal(output, engine_rpm)
