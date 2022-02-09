23c9eb4733fe95e060ce45857d6915a515c0e80b
Quinn Thibeault <quinn.thibeault96@gmail.com>
2022-02-09 10:08:09 -0700

    Create test case for 2D lookup table using engine data

---
+++ tests/autotrans/test_lookup_table.py
@@ -0,0 +1,12 @@
+from autotrans.lookup_table import LookupTable2D
+from autotrans.engine import THROTTLE_BREAKPOINTS, RPM_BREAKPOINTS, ENGINE_TORQUE_TABLE_VALUES
+from pytest import approx
+
+
+def test_lookup_table_2d():
+    table = LookupTable2D(THROTTLE_BREAKPOINTS, RPM_BREAKPOINTS, ENGINE_TORQUE_TABLE_VALUES)
+
+    assert table.lookup(60.0000, 1000.0000) == approx(278.5000, abs=0.01)
+    assert table.lookup(59.9463, 1383.2808) == approx(291.3155, abs=0.01)
+    assert table.lookup(59.8926, 1685.3620) == approx(293.7103, abs=0.01)
+    assert table.lookup(59.8389, 1907.2317) == approx(295.7509, abs=0.01)
