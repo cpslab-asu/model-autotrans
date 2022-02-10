f443240daf9a40e93adfb8666326d8d00f84d82e
Quinn Thibeault <quinn.thibeault96@gmail.com>
2022-02-10 08:09:37 -0700

    Update ShiftLogic test. Check all outputs at once for better error reporting. Step and then record gear since each step requires the inputs from the last time step

--- tests/autotrans/test_shift_logic.py
+++ tests/autotrans/test_shift_logic.py
@@ -1,8 +1,6 @@
 from os import path
-from pprint import pprint
 
 import h5py
-import numpy as np
 import pytest
 
 from autotrans.shift_logic import ShiftLogic, Gear, up_shift_threshold, down_shift_threshold
@@ -29,20 +27,14 @@ def test_shift_logic():
     throttle_trace = test_data["throttle"]
     vehicle_speed_trace = test_data["vehicle_speed"]
     gear_trace = test_data["gear"]
-    model = ShiftLogic(
-        wait_ticks=2,
-        initial_gear=Gear.FIRST,
-        initial_throttle=throttle_trace[0],
-        initial_speed=vehicle_speed_trace[0]
-    )
-    inputs = zip(throttle_trace, vehicle_speed_trace, gear_trace)
-    output_trace = []
+    model = ShiftLogic(wait_ticks=2, initial_gear=Gear.FIRST)
+    inputs = zip(throttle_trace[0:750], vehicle_speed_trace[0:750])
+    outputs = []
 
-    for throttle, vehicle_speed, gear in inputs:
-        output_trace.append(model.current_gear)
+    for throttle, vehicle_speed in inputs:
         model.step(throttle, vehicle_speed)
-        # assert model.current_gear == gear
+        outputs.append(model.current_gear)
 
-    times = np.linspace(start=0, stop=30, num=len(output_trace), endpoint=True)
-    print()
-    pprint(list(zip(times, output_trace, gear_trace)), width=120)
+    outputs.append(model.current_gear)
+
+    assert outputs == list(gear_trace)
