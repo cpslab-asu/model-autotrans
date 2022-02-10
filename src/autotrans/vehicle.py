604bb1c713cc63b5969bbdd6ecc2a68ed7fdc6a1
Quinn Thibeault <quinn.thibeault96@gmail.com>
2022-02-10 16:52:57 -0700

    First revision for vehicle behavior

--- src/autotrans/vehicle.py
+++ src/autotrans/vehicle.py
@@ -1,10 +1,11 @@
 import math
 
+import numpy as np
 from scipy import integrate
 
 
-def _into_mph(speed: float) -> float:
-    ...
+def _into_mph(feet_per_min: float) -> float:
+    return feet_per_min * 60 / 5280
 
 
 class Vehicle:
@@ -13,25 +14,31 @@ class Vehicle:
 
     def __init__(
         self,
+        t_step_ms: int,
         final_drive_ratio: float,
         co_friction: float,
         co_drag: float,
         wheel_radius: float,
         inertia: float,
-        initial_transmission_rpm: float,
-        initial_vehicle_speed: float
+        initial_speed: float
     ):
+        # Parameters
+        self._time_step = t_step_ms / 1000
         self._final_drive_ratio = final_drive_ratio
         self._co_friction = co_friction
         self._co_drag = co_drag
         self._wheel_radius = wheel_radius
         self._inertia = inertia
-        self._transmission_rpm = initial_transmission_rpm
-        self._speed = initial_vehicle_speed
+
+        # State variables
+        initial_wheel_speed = initial_speed / wheel_radius
+        self._wheel_speed = initial_wheel_speed
+        self._transmission_rpm = initial_wheel_speed * final_drive_ratio
+        self._speed = _into_mph(initial_wheel_speed)
         self._signed_load = 0.0
 
     def _compute_speed(self, wheel_speed: float) -> float:
-        return _into_mph(wheel_speed * 2 * math.pi * self._wheel_radius)
+        return
 
     def _compute_signed_load(self, vehicle_speed: float, brake: float) -> float:
         load = vehicle_speed**2 * self._co_drag + self._co_friction
@@ -40,20 +47,22 @@ class Vehicle:
         return (load + brake) * sign
 
     def step(self, output_torque: float, brake: float):
-        final_drive_ratio = output_torque * self._final_drive_ratio
-        vehicle_inertia = (final_drive_ratio * self._signed_load) / self._inertia
-        # TODO: Implement integrator for vehicle inertia
-        integration = integrate.solve_ivp()
-        wheel_speed = integration["y"][-1]
-
-        self._transmission_rpm = self._final_drive_ratio * wheel_speed
-        self._speed = self._compute_speed(wheel_speed)
         self._signed_load = self._compute_signed_load(self._speed, brake)
+
+        drive_ratio = output_torque * self._final_drive_ratio
+        vehicle_inertia = (drive_ratio + self._signed_load) / self._inertia
+        integration = integrate.solve_ivp(
+            fun=lambda t, o: vehicle_inertia,
+            t_span=(0, self._time_step),
+            y0=np.array([self._wheel_speed], dtype=np.float64)
+        )
+
+        self._wheel_speed = integration["y"][-1, -1]
 
     @property
     def transmission_rpm(self) -> float:
-        return self._transmission_rpm
+        return self._final_drive_ratio * self._wheel_speed
 
     @property
     def speed(self) -> float:
-        return self._speed
+        return _into_mph(self._wheel_speed * 2 * math.pi * self._wheel_radius)
