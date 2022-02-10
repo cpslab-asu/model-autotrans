48c6794ab20cd32fee9311d5aeaa455ab25c6c52
Quinn Thibeault <quinn.thibeault96@gmail.com>
2022-02-09 22:02:43 -0700

    Update engine test to check each iteration instead of all at the end

--- tests/autotrans/test_engine.py
+++ tests/autotrans/test_engine.py
@@ -1,49 +1,25 @@
 import os.path as path
 
 import h5py
-import numpy as np
-import numpy.testing
-import pytest
 
 import autotrans.engine as engine
 
 INERTIA = 0.021991488283555904
 
 
-@pytest.fixture
-def trace_data() -> h5py.File:
-    return h5py.File(path.join(path.dirname(__file__), "test_data.h5"))
-
-
-def test_engine_step(trace_data: h5py.File):
-    engine_rpm = trace_data["engine_rpm"]
-    throttle = trace_data["throttle"]
-    impeller_torque = trace_data["impeller_torque"]
-    expected_output = engine_rpm[1]
-
-    model = engine.Engine(
-        time_step_ms=40,
-        engine_propeller_inertia=INERTIA,
-        initial_rpm=engine_rpm[0]
-    )
-    model.step(throttle=throttle[0], impeller_torque=impeller_torque[0])
-
-    assert model.rpm == expected_output
-
-
-def test_engine_steps(trace_data: h5py.File):
-    throttle_trace: h5py.Dataset = trace_data["throttle"]
-    impeller_torque_trace: h5py.Dataset = trace_data["impeller_torque"]
-    engine_rpm: h5py.Dataset = trace_data["engine_rpm"]
+def test_engine():
+    test_data = h5py.File(path.join(path.dirname(__file__), "test_data.h5"))
+    throttle_trace = test_data["throttle"]
+    impeller_torque_trace = test_data["impeller_torque"]
+    engine_rpm = test_data["engine_rpm"]
     model = engine.Engine(
         time_step_ms=40,
         engine_propeller_inertia=INERTIA,
         initial_rpm=engine_rpm[0],
     )
-    output = []
+    inputs = zip(throttle_trace, impeller_torque_trace, engine_rpm)
 
-    for throttle, impeller_torque in zip(throttle_trace, impeller_torque_trace):
-        output.append(model.rpm)
+    for throttle, impeller_torque, engine_rpm in inputs:
+        assert model.rpm == engine_rpm
+
         model.step(throttle, impeller_torque)
-
-    assert np.testing.assert_equal(output, engine_rpm)
