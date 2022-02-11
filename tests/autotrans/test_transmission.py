b94ad5eb9c0bc14151b981f65fa1d8c0352f6162
Quinn Thibeault <quinn.thibeault96@gmail.com>
2022-02-11 14:58:32 -0700

    Separate transmission output tests to test both simultaneously

--- tests/autotrans/test_transmission.py
+++ tests/autotrans/test_transmission.py
@@ -4,24 +4,35 @@ import pytest
 from autotrans.transmission import Transmission
 
 
-def test_transmission(test_data: h5py.File):
+def test_transmission_impeller_torque(test_data: h5py.File):
+    transmission_rpm_trace = test_data["transmission_rpm"]
+    engine_rpm_trace = test_data["engine_rpm"]
+    gear_trace = test_data["gear"]
+    impeller_torque_trace = test_data["impeller_torque"]
+
+    model = Transmission()
+    inputs = zip(engine_rpm_trace, gear_trace, transmission_rpm_trace)
+    outputs = []
+
+    for engine_rpm, gear, transmission_rpm in inputs:
+        model.step(engine_rpm, gear, transmission_rpm)
+        outputs.append(model.impeller_torque)
+
+    assert outputs == pytest.approx(list(impeller_torque_trace), abs=1.0e-3)
+
+
+def test_transmission_output_torque(test_data: h5py.File):
     transmission_rpm_trace = test_data["transmission_rpm"]
     engine_rpm_trace = test_data["engine_rpm"]
     gear_trace = test_data["gear"]
     output_torque_trace = test_data["output_torque"]
-    impeller_torque_trace = test_data["impeller_torque"]
 
     model = Transmission()
-    inputs = zip(
-        engine_rpm_trace,
-        gear_trace,
-        transmission_rpm_trace,
-        output_torque_trace,
-        impeller_torque_trace
-    )
+    inputs = zip(engine_rpm_trace, gear_trace, transmission_rpm_trace)
+    outputs = []
 
-    for engine_rpm, gear, transmission_rpm, output_torque, impeller_torque in inputs:
+    for engine_rpm, gear, transmission_rpm in inputs:
         model.step(engine_rpm, gear, transmission_rpm)
+        outputs.append(model.output_torque)
 
-        assert model.output_torque == pytest.approx(output_torque, abs=1.0e-3)
-        assert model.impeller_torque == pytest.approx(impeller_torque, abs=1.0e-3)
+    assert outputs == pytest.approx(list(output_torque_trace), abs=1.0e-3)
