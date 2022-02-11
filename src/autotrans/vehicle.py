77e74663efb96fb9bd57f8c7ca4109091ac0b31b
Quinn Thibeault <quinn.thibeault96@gmail.com>
2022-02-11 15:01:03 -0700

    Rename co_friction to wheel_friction. Convert from meters to miles per hour. Compute initial wheel speed in constructor. Fix sign of signed load in inertia computation

--- src/autotrans/vehicle.py
+++ src/autotrans/vehicle.py
@@ -5,18 +5,15 @@ from scipy import integrate
 
 
 def _into_mph(feet_per_min: float) -> float:
-    return feet_per_min * 60 / 5280
+    return feet_per_min * 60 / 1609.34
 
 
 class Vehicle:
-    RLOAD0 = None
-    RLOAD2 = None
-
     def __init__(
         self,
         t_step_ms: int,
         final_drive_ratio: float,
-        co_friction: float,
+        wheel_friction: float,
         co_drag: float,
         wheel_radius: float,
         inertia: float,
@@ -25,32 +22,29 @@ class Vehicle:
         # Parameters
         self._time_step = t_step_ms / 1000
         self._final_drive_ratio = final_drive_ratio
-        self._co_friction = co_friction
+        self._wheel_friction = wheel_friction
         self._co_drag = co_drag
         self._wheel_radius = wheel_radius
         self._inertia = inertia
 
         # State variables
-        initial_wheel_speed = initial_speed / wheel_radius
-        self._wheel_speed = initial_wheel_speed
-        self._transmission_rpm = initial_wheel_speed * final_drive_ratio
-        self._speed = _into_mph(initial_wheel_speed)
+        self._wheel_speed = initial_speed / wheel_radius
         self._signed_load = 0.0
 
     def _compute_speed(self, wheel_speed: float) -> float:
         return
 
     def _compute_signed_load(self, vehicle_speed: float, brake: float) -> float:
-        load = vehicle_speed**2 * self._co_drag + self._co_friction
+        load = vehicle_speed**2 * self._co_drag + self._wheel_friction
         sign = math.copysign(1.0, vehicle_speed)
 
         return (load + brake) * sign
 
     def step(self, output_torque: float, brake: float):
-        self._signed_load = self._compute_signed_load(self._speed, brake)
+        self._signed_load = self._compute_signed_load(self.speed, brake)
 
         drive_ratio = output_torque * self._final_drive_ratio
-        vehicle_inertia = (drive_ratio + self._signed_load) / self._inertia
+        vehicle_inertia = (drive_ratio - self._signed_load) / self._inertia
         integration = integrate.solve_ivp(
             fun=lambda t, o: vehicle_inertia,
             t_span=(0, self._time_step),
