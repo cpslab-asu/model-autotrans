89a04ab55830c343fe54c9c2d7d3eac77f707437
Quinn Thibeault <quinn.thibeault96@gmail.com>
2022-03-21 15:02:30 -0700

    Create test for engine impeller inertia

--- tests/autotrans/test_engine.py
+++ tests/autotrans/test_engine.py
@@ -5,7 +5,26 @@ import autotrans.engine as engine
 INERTIA = 0.021991488283555904
 
 
-def test_engine(test_data: h5py.File):
+def test_engine_impeller_inertia(test_data: h5py.File):
+    throttle_trace = test_data["throttle"]
+    impeller_torque_trace = test_data["impeller_torque"]
+    rpm_trace = test_data["engine_rpm"]
+    engine_impeller_inertia = test_data["engine_impeller_inertia"]
+    model = engine.Engine(
+        time_step_ms=40,
+        engine_propeller_inertia=INERTIA,
+        initial_rpm=rpm_trace[0]
+    )
+    inputs = zip(throttle_trace, impeller_torque_trace, rpm_trace)
+    outputs = []
+
+    for throttle, impeller_torque, rpm in inputs:
+        outputs.append(model.engine_impeller_inertia(throttle, impeller_torque, rpm))
+
+    assert outputs == list(engine_impeller_inertia)
+
+
+def test_engine_rpm(test_data: h5py.File):
     throttle_trace = test_data["throttle"]
     impeller_torque_trace = test_data["impeller_torque"]
     engine_rpm = test_data["engine_rpm"]
@@ -14,9 +33,11 @@ def test_engine(test_data: h5py.File):
         engine_propeller_inertia=INERTIA,
         initial_rpm=engine_rpm[0],
     )
-    inputs = zip(throttle_trace, impeller_torque_trace, engine_rpm)
+    inputs = zip(throttle_trace, impeller_torque_trace)
+    outputs = []
 
-    for throttle, impeller_torque, engine_rpm in inputs:
-        assert model.rpm == engine_rpm
-
+    for throttle, impeller_torque in inputs:
+        outputs.append(model.rpm)
         model.step(throttle, impeller_torque)
+
+    assert outputs == list(engine_rpm)
